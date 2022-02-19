760137a880852499582a5d87ad8a54027f83697a
mikeenda2 <89474945+mikeenda2@users.noreply.github.com>
2022-02-18 22:54:22 -0600

    Update personsList.java

--- personsList.java
+++ personsList.java
@@ -1,5 +1,15 @@
 package com.iffi;
 
 public class personsList {
-
+  private List<Object> persons;
+	
+	public personsList () {
+		this.persons = new ArrayList<Object>();
+	}
+	public List<Object> getPersonsList(){
+		return this.persons;
+	}
+	public void addPerson(Object Person) {
+		this.persons.add(Person);
+	}
 }
